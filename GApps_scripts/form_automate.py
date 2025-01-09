a5bd512067ce9a544d38ed363d11d53e706d9304
adhyuthn <adhyuthnarayan008@gmail.com>
2025-01-09 14:39:06 +0530

    automate test 1

--- GApps_scripts/form_automate.py
+++ GApps_scripts/form_automate.py
@@ -4,9 +4,10 @@ import os
 API_KEY = os.environ["API_KEY_SHEETS"]
 SPREADSHEET_ID = os.environ["SPREADSHEET_ID_SHEETS"]
 range_to_read = 'A2:C'
-url = f'https://sheets.googleapis.com/v4/spreadsheets/{SPREADSHEET_ID}/values/{range_to_read}?key={API_KEY}'
 
-response = requests.get(url)
+read_url = f'https://sheets.googleapis.com/v4/spreadsheets/{SPREADSHEET_ID}/values/{range_to_read}?key={API_KEY}'
+
+response = requests.get(read_url)
 last_entry = response.json()["values"][-1]
 time_text    = last_entry[0]
 heading_text = last_entry[1]
@@ -14,7 +15,7 @@ content_text = last_entry[2]
 
 print("RESPONSE:\n", response.json())
 
-with open("TEST_FORM.md", "a") as fd:
+with open("tests/TEST_FORM.md", "a") as fd:
       fd.write(f"""
 # {heading_text}
 
